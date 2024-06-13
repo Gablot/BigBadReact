fd82d5da03d4f5bb3cb4c939bb24adddf3164d09
Gablot <khylecarabio@gmail.com>
2024-06-13 22:20:11 +0800

    Tydying up builds and enabling turbo

--- t3gallery/next.config.js
+++ t3gallery/next.config.js
@@ -1,3 +1,5 @@
+import { ESLint } from "eslint";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -5,6 +7,13 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
 
 export default config;
